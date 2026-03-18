6bb2639ab314ead1a1dbe12a9fd2db0eb422f908
Karol Stanislav <s.karol@jsa-group.ru>
2026-03-18 11:36:04 +0300

    chore: использовать FSD для приложения

--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -1,11 +1,12 @@
-import type { Configuration } from 'webpack';
+import type { Configuration } from "webpack";
+import path from "path";
 
-import { rules } from './webpack.rules';
-import { plugins } from './webpack.plugins';
+import { rules } from "./webpack.rules";
+import { plugins } from "./webpack.plugins";
 
 rules.push({
   test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+  use: [{ loader: "style-loader" }, { loader: "css-loader" }],
 });
 
 export const rendererConfig: Configuration = {
@@ -14,6 +15,10 @@ export const rendererConfig: Configuration = {
   },
   plugins,
   resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".css"],
+    alias: {
+      "@renderer": path.resolve(__dirname, "src/renderer"),
+      "@shared": path.resolve(__dirname, "src/shared"),
+    },
   },
 };
