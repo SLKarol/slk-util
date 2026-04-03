ffdc19bc1be58c5a9655ccfaba49b7ba82a54c26
Karol Stanislav <s.karol@jsa-group.ru>
2026-04-03 12:48:11 +0300

    feat: статус disabled

--- src/renderer/widgets/stihi-ru/ui/EnterPage/ButtonRunEnter.tsx
+++ src/renderer/widgets/stihi-ru/ui/EnterPage/ButtonRunEnter.tsx
@@ -1,16 +1,26 @@
 import { Button, Tooltip } from "@mantine/core";
+import { observer } from "mobx-react-lite";
+
+import { useStihiRuRootStore } from "@renderer/providers/stihi-ru/useStihiRuRootStore";
 
 /**
  * Кнопка запуска процесса открытия произведений.
  */
-export const ButtonRunEnter = () => {
+export const ButtonRunEnter = observer(() => {
+  const {
+    stihiRuUiStore: { browserProgramEntered },
+  } = useStihiRuRootStore();
+
   const onClick = () => {
     console.log(window.electronAPI);
   };
 
   return (
     <Tooltip label="Запустить процесс открытия произведений. Пригласить так сказать.">
-      <Button onClick={onClick}>Запустить</Button>
+      <Button onClick={onClick} disabled={!browserProgramEntered}>
+        Запустить
+      </Button>
     </Tooltip>
   );
-};
+});
+ButtonRunEnter.displayName = "ButtonRunEnter";
