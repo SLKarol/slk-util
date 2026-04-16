40adf28140edb82dbef2c959608fc946d94a26f0
Karol Stanislav <s.karol@jsa-group.ru>
2026-04-16 17:17:46 +0300

    fix: исправлена ошибка определения доступности кнопки "Зайти на страницу"

--- src/renderer/widgets/stihi-ru/ui/EnterPage/ButtonRunEnter.tsx
+++ src/renderer/widgets/stihi-ru/ui/EnterPage/ButtonRunEnter.tsx
@@ -1,4 +1,5 @@
 import { Button, Tooltip } from "@mantine/core";
+import { notifications } from "@mantine/notifications";
 import { observer } from "mobx-react-lite";
 
 import { useStihiRuRootStore } from "@renderer/providers/stihi-ru/useStihiRuRootStore";
@@ -16,15 +17,26 @@ export const ButtonRunEnter = observer(() => {
   } = useRootStore();
 
   const onClick = () => {
+    if (!dateExists)
+      return notifications.show({
+        title: "Ошибка",
+        message: "Не выбрана дата",
+        color: "red",
+      });
+
+    if (!browserProgramEntered)
+      return notifications.show({
+        title: "Ошибка",
+        message: "Не выбран браузер",
+        color: "red",
+      });
+
     if (dateValue) window.electronAPI.startStihiAutoRead(dateValue);
   };
 
   return (
     <Tooltip label="Запустить процесс открытия произведений. Пригласить так сказать.">
-      <Button
-        onClick={onClick}
-        disabled={isTracking || !browserProgramEntered || !dateExists}
-      >
+      <Button onClick={onClick} disabled={isTracking}>
         Запустить
       </Button>
     </Tooltip>
