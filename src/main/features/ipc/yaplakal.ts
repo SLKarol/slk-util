02a2b21063ce13c1aad799f4fd66cfc0109f9916
Karol Stanislav <s.karol@jsa-group.ru>
2026-04-17 17:51:23 +0300

    feat: begin parse

--- src/main/features/ipc/yaplakal.ts
+++ src/main/features/ipc/yaplakal.ts
@@ -1,4 +1,10 @@
-import { type IpcMainEvent } from "electron";
+import { app, type IpcMainEvent } from "electron";
+import { mkdir } from "fs/promises";
+import { parse } from "node-html-parser";
+import { join } from "path";
+
+import { TMP_FOLDER } from "../lib/constants";
+import { fetchHtml } from "../lib/fetch";
 
 import { CHANNELS } from "@shared/ipc/channels";
 
@@ -6,10 +12,24 @@ import { CHANNELS } from "@shared/ipc/channels";
  * Обработчики IPC-запросов для работы с Yaplakal.
  */
 export const yapHandlers = {
-  [CHANNELS.FETCH_YA_PLAKAL_TOPIC]: (
+  [CHANNELS.FETCH_YA_PLAKAL_TOPIC]: async (
     ipcMainEvent: IpcMainEvent,
     url: string,
   ) => {
+    const tmpFolder = join(app.getPath("temp"), TMP_FOLDER);
+    await mkdir(tmpFolder, { recursive: true });
+    try {
+      const htmlText = await fetchHtml(url);
+      const rootPage = parse(htmlText);
+    } catch (error) {
+      console.error("Error:", error);
+      ipcMainEvent.reply(CHANNELS.ERROR_MAIN, {
+        requestParam: CHANNELS.FETCH_YA_PLAKAL_TOPIC,
+        error,
+      });
+    }
+
     console.log("url :>> ", url);
+    console.log("tmpFolder :>> ", tmpFolder);
   },
 };
--- src/main/features/lib/constants.ts
+++ src/main/features/lib/constants.ts
@@ -2,3 +2,8 @@
  * Base URL stihi.ru
  */
 export const BASE_URL_STIHI_RU = "https://stihi.ru";
+
+/**
+ * Имя временного каталога
+ */
+export const TMP_FOLDER = "bf6c4f42-49df-48a7-9a2f-fa64de34f444";
